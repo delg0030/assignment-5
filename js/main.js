77d3f3e1c0f556e810f883e074b528899f864102
Brittany <delgatyb@gmail.com>
2015-02-28 10:01:00 -0500

    completed tutorial

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 var $form = $('.form');
+var $name = $('.name');
+var $age = $('.age');
+var $dinos = $('.dinos');
 
 
 
-$form.on('submit', function(e) {  
+$form.on('submit', function (e) {  
     e.preventDefault();
-    console.log('submitted!')'
+    
+var $li = $('<li>');    
+var $nameH2 = $('<h2>').html($name.val());
+var $ageP = $('<p>').html($age.val());
+    
+$li.append($nameH2).append($ageP);
+$dinos.append($li);
+
 });
 
 
