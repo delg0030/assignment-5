ff7aff9e303c44a204dd508c37b60c5fd02e8190
Brittany <delgatyb@gmail.com>
2015-03-01 10:03:01 -0500

    some css and text decoration added

--- js/main.js
+++ js/main.js
@@ -5,18 +5,22 @@ var $dinos = $('.dinos');
 
 
 
+
 $form.on('submit', function (e) {  
     e.preventDefault();
     
 var $li = $('<li>');    
-var $nameH2 = $('<h2>').html($name.val());
+var $nameH4 = $('<h4>').html($name.val());
 var $ageP = $('<p>').html($age.val());
     
-$li.append($nameH2).append($ageP);
+$li.append($nameH4).append($ageP);
 $dinos.append($li);
 
 });
 
+$dinos.on('click', 'li', function() {
+    $(this).addClass('js-strike');
+});
 
 
 
