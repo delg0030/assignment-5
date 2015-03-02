982c27ca5b4042f6ce44f489a6cf2d21632bb35c
Brittany <delgatyb@gmail.com>
2015-03-02 16:07:12 -0500

    fixed and finished delete buttons complete

--- js/main.js
+++ js/main.js
@@ -3,9 +3,6 @@ var $name = $('.name');
 var $age = $('.age');
 var $dinos = $('.dinos');
 
-
-
-
 $form.on('submit', function (e) {  
     e.preventDefault();
     
@@ -13,12 +10,16 @@ var $li = $('<li>');
 var $nameH4 = $('<h4>').html($name.val());
 var $ageP = $('<p>').html($age.val());
 var $button = $('<button>');
+var $button = $('<button class="delete">X</button>')
+
+$li.append($button);
+
 
   $button.html('&#x2717');
-  $button.addClass('done');
+  $button.addClass('delete');
 
   $button.on('click', function(){
-    $li.remove();
+    $(this).parent().remove();
   })
     
 $li.append($nameH4).append($ageP);
@@ -27,11 +28,6 @@ $addH2.append($button);
 
 });
 
-
 $dinos.on('click', 'li', function () {
     $(this).toggleClass('strike');
 });
-
-
-
-
